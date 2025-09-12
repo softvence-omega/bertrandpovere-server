e452ef44a0295a9ea38ecddf2188e1e97a405b33
md-maruf-billa <eng.marufbilla@gmail.com>
2025-09-12 14:38:18 +0600

    added-: added server initial setup

---
+++ readme.md
@@ -0,0 +1,22 @@
+# Project Name : `bertrandpovere`
+
+Live URL: *https://*
+
+---
+
+
+## 📑 Table of Contents
+
+- [Data Types](#data-types)
+    - [Account Model](#account-model)
+
+ ---
+
+
+## Data Types 
+
+### Account Model
+```ts
+
+
+```
---
+++ src/app/modules/auth/auth.interface.ts
@@ -0,0 +1,24 @@
+export type TAccount = {
+    email: string;
+    password: string;
+    lastPasswordChange?: Date;
+    isDeleted?: boolean;
+    accountStatus?: "ACTIVE" | "INACTIVE" | "SUSPENDED";
+    role?: "USER" | "ADMIN",
+    isVerified?: boolean,
+}
+
+
+export interface TRegisterPayload extends TAccount {
+    name: string
+}
+
+export type TLoginPayload = {
+    email: string;
+    password: string
+}
+
+export type TJwtUser = {
+    email: string,
+    role?: "USER" | "ADMIN",
+}
