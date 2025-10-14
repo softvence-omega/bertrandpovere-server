a88c589ca5322f1809c53639fa853274ec7bbd78
abumahid <eng.marufbilla@gmail.com>
2025-10-14 09:47:38 +0600

    fixed socket

--- src/server.ts
+++ src/server.ts
@@ -10,8 +10,8 @@ let io: Server;
 const server = http.createServer(app);
 io = new Server(server, {
     cors: {
-        origin: ["http://localhost:5173", "https://unnig-courier.vercel.app"],
-        methods: ["GET", "POST"],
+        origin: ["http://localhost:5173", "http://localhost:5174", "http://localhost:3000"],
+        methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
         credentials: true,
     },
 });
