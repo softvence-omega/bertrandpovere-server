33183e4968f2b4057b065f98353f04b2e078ceeb
abumahid <eng.marufbilla@gmail.com>
2025-10-27 11:18:55 +0600

    update

--- src/app.ts
+++ src/app.ts
@@ -29,7 +29,7 @@ app.use("/api", appRouter)
 app.get('/', (req: Request, res: Response) => {
     res.status(200).json({
         status: 'success',
-        message: 'Server is running successful !!',
+        message: 'Hello now successfully added ci cd on this server',
         data: null,
     });
 });
