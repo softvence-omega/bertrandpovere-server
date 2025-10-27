f59c498607901beef2bb0c28793cb3af5f937e1a
abumahid <eng.marufbilla@gmail.com>
2025-10-27 14:36:23 +0600

    update

--- src/app.ts
+++ src/app.ts
@@ -29,7 +29,7 @@ app.use("/api", appRouter)
 app.get('/', (req: Request, res: Response) => {
     res.status(200).json({
         status: 'success',
-        message: 'This is the starting point, Make a request to /api for all endpoints',
+        message: 'Welcome to the API',
         data: null,
     });
 });
