50f7260833ddf58bf65edb2970ddb49ae359b3aa
Abumahid Islam <eng.marufbilla@gmail.com>
2025-10-31 15:23:45 +0600

    update site

--- src/app.ts
+++ src/app.ts
@@ -14,7 +14,8 @@ app.use(cors({
         "http://localhost:3000",
         "http://localhost:5173",
         "http://localhost:5174",
-        "https://mellifluous-selkie-e0d36e.netlify.app"
+        "https://mellifluous-selkie-e0d36e.netlify.app",
+        "https://main.d1yhx6z9xvjtzi.amplifyapp.com"
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"]
